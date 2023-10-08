44b42a8bc1beccdfa14be0176d890c5c7d09ad52
Noureddine Achbili <146972598+N0urDEV@users.noreply.github.com>
2023-10-08 18:57:28 +0100

    Add files via upload

---
+++ a program that determines the sign of two numbers.py
@@ -0,0 +1,6 @@
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+if a >= 0 and b >= 0 or a < 0 and b < 0 :
+    print("The two numbers has the same sign")
+else : 
+    print("The two number has a different sign")
